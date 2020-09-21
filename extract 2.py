0c1791ab5b06efdd50b17bb723caf1ed34a309be
Alkım Deniz Altıntaş <altalk.23@robcol.k12.tr>
2020-09-21 16:24:30 +0300

    api.newgrounds.app is down / changed to beautifulsoup

--- extract 2.py
+++ extract 2.py
@@ -10,7 +10,6 @@ from eyed3 import id3
 import eyed3
 from html2text import html2text
 from progress.bar import Bar
-from bs4 import BeautifulSoup
 
 # Get the current working directory
 cwdir = os.getcwd()
@@ -22,7 +21,7 @@ homedir = os.path.expanduser("~")
 musicdir = os.path.join(cwdir, "music")
 if not os.path.exists(musicdir):
     os.mkdir(musicdir)
-
+    
 # Get the cache directory
 if sys.platform == "darwin":
     cachedir = os.path.join(homedir, "Library", "Caches")
@@ -47,7 +46,7 @@ if os.path.exists(previouspath):
         musiclist = pickle.load(file)
 else:
     musiclist = []
-
+    
 # Do for each id
 for id in idlist:
 
@@ -55,64 +54,55 @@ for id in idlist:
     if id not in musiclist:
 
         # Set the url for requesting
-        ngurl = f"http://newgrounds.com/audio/listen/{id}"
-
+        ngurl = f"url=newgrounds.com/audio/listen/{id}"
+        apiurl = f"https://api.newgrounds.app/details.php?{ngurl}"
         try:
             # Request the content
-            content = requests.get(ngurl)
-            #print(content.text)
-            soup = BeautifulSoup(content.text, 'html.parser')
-
-            # Check if you are living in Turkey / I still don't understand why it's blocked
-            if "Bilgi Teknolojileri" in soup.title.text:
-                print()
-                print("whyyyyyyyyyyyyyyyyyy :(((((((")
-                break
-
+            content = requests.get(apiurl).json()
             # Get metadata
-            title = soup.title.text
-            artist = soup.find("div", {"class": "item-details-main"}).contents[1].contents[1].text
-            comment = soup.find(id="author_comments").text
-            genre = soup.find(attrs={"data-genre-for": True}).text
-            releasedate = soup.find(id="sidestats").contents[5].contents[3].text[-4:]
-            imageurl = f"https://aicon.ngfiles.com/{int(id)//1000}/{id}.png"
-
+            title = content["audio_details"][0]["name"]
+            artist = content["audio_details"][0]["artist"]
+            comment = html2text(content["description"][0]["details"])
+            genre = content["information"][0]["genre"]
+            releasedate = content["information"][0]["uploaded"]
+            imageurl = content["audio_details"][0]["icon"]
+            
             contact = True
-
-
+            
         except:
             # The music is deleted or removed
             contact = False
-
+           
             # Get file locations
             cachefile = os.path.join(cachedir, f"{id}.mp3")
             musicfile = os.path.join(musicdir, f"Unknown [{id}].mp3")
 
             # Copy the music file to the directory
             copy(cachefile, musicfile)
-
+            
         if contact:
-
+        
             # Remove special characters from title
             title = re.sub('[<>:"/\\|?*]', "", title)
-
-            # Set year
-            year = eyed3.core.Date(int(releasedate))
-
+                
+            # Get year from unix timestamp
+            uploadtime = datetime.fromtimestamp(releasedate)
+            year = eyed3.core.Date(uploadtime.year)
+            
             # Get file locations
             cachefile = os.path.join(cachedir, f"{id}.mp3")
             musicfile = os.path.join(musicdir, f"{title} [{id}].mp3")
-
+            
             # Check if file already exists
             if not os.path.exists(musicfile):
                 # Copy the music file to the directory
                 copy(cachefile, musicfile)
-
+                
             # Create mp3 tag
             tag = id3.tag.Tag()
             tag.parse(musicfile)
             tag.header.version = id3.ID3_V2_3
-
+            
             # Set tag values
             tag.artist = artist
             tag.title = title
@@ -120,24 +110,23 @@ for id in idlist:
             tag.genre = genre
             tag.recording_date = year
             tag.comments.set(comment)
-
+            
             try:
                 # Get image data
                 response = requests.get(imageurl)
                 imagedata = response.content
-
+                
                 # Set image data
                 tag.images.set(type_=3, img_data=imagedata, mime_type="image/png")
-
+                
             except:
                 pass
 
             # Save the tag
             tag.save(encoding="utf-8")
-
-
+        
     bar.next()
-
+    
 # Update previous music ID list
 try:
     musiclist += [re.findall("\\[\\d+\\]", f)[0][1:-1] for f in os.listdir(musicdir) if f[:7] != "Unknown"]
